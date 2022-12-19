58a4a5f69f365fb9174d611b474daeb14ce3da90
Vance97 <vansmuchongo@gmail.com>
2022-12-19 20:05:40 +0200

    composer, php installed

--- mail_handler.php
+++ mail_handler.php
@@ -1,26 +1,20 @@
 <?php
+require 'PHPMailerAutoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-	if(isset($_POST['submit'])){
-		$name=$_POST['name'];
-		$email=$_POST['email'];
-		$contact_number=$_POST['contact-number'];
-		$subject=$_POST['subject'];
-		$comment=$_POST['comment'];
-
-		$to='muchogo.vance@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='A message from puno.africa/contact page';
-		$message="Name :".$name."\n"."Phone :".$contact_number."\n"."Wrote the following :"."\n\n".$comment;
-		$headers="From: ".$email;
-
-		if(mail($to, $subject, $comment, $headers)){
-			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
-	}
-?>
+$mail = new PHPMailer;
+$mail->setFrom('from@example.com', 'Your Name');
+$mail->addAddress('myfriend@example.net', 'My Friend');
+$mail->Subject  = 'First PHPMailer Message';
+$mail->Body     = 'Hi! This is my first e-mail sent through PHPMailer.';
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Message has been sent.';
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '689b6d09a91f50e25f5cd8c8e03f9d40433e823e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '689b6d09a91f50e25f5cd8c8e03f9d40433e823e',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.7.1',
+      'version' => '6.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '49cd7ea3d2563f028d7811f06864a53b1f15ff55',
+    ),
+  ),
+);
